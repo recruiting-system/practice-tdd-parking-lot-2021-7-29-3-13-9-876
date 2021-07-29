05ce2839d3dcb4a4451cb7cd628e08d03f9e69bf
linwenjun <linwenjun@163.com>
2021-07-29 03:13:19 +0000

    first commit

---
+++ src/main/java/com/parkinglot/ParkingBoy.java
@@ -0,0 +1,5 @@
+package com.parkinglot;
+
+public class ParkingBoy {
+
+}
---
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -0,0 +1,13 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParkingBoyTest {
+
+    @Test
+    void write_your_first_test() {
+
+    }
+}
